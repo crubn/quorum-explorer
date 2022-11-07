bc7bde0eec73f73648ead4c478fda72327e22c5f
aashishPrasad-crubn <aashish.prasad@crubn.com>
2022-11-07 16:37:25 +0530

    Add open graph meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,9 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/explorer/images/favicon.png" />
+          <meta property="og:title" content="Quorum Explorer | CRUBN" />
+          <meta property="og:description" content="Explore the Quorum blockchain in real time" />
+          <meta property="og:image" content="/explorer/images/og-image.png" />
         </Head>
         <body>
           {/* 👇 Here's the script */}
